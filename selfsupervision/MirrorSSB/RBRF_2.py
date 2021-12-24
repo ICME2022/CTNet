e64019c74b1fb773fbd7a8b6861281e664d34497
ICME2022 <96417027+ICME2022@users.noreply.github.com>
2021-12-24 21:00:54 +0800

    Update RBRF_2.py

--- selfsupervision/MirrorSSB/RBRF_2.py
+++ selfsupervision/MirrorSSB/RBRF_2.py
@@ -39,55 +39,41 @@ def TmirrorHeng(image):
     return image[:, index]
 
 
-#RBRF核心算法
-#此处的RBRF只保留两个类别,但是实际在变化的时候还是会分成5类去进行变换
+
 def RBRF(images):#image->[BT,C,H,W]
     image = create5Dimages(images)#image->[B,T,C,H,W]
     B,T,C,H,W = image.size()
-    #生成batch级标签-1-level标签
+
     labelb = [random.randint(0, 1) for _ in range(B)]
     label_B = torch.tensor(labelb).cuda()
-    #创建总标签
+
     labels = torch.tensor([]).long().cuda()
-    #创建总的结果集
+
     mirrorImage = torch.tensor([]).cuda()
     
     
     for l in range(B):
-        #创建保存每个B内T帧标签的集合
+
         label_1_t = torch.tensor([]).long().cuda()
         if(label_B[l]==0):
-            #当前batch没有被选中，此时，该batch内的T帧不做任何的变动，并且每一帧的伪标签为0
-            #首先直接给最终的标签集合cat T个0
             labels = torch.cat([labels,torch.zeros(8).long().cuda()])
-            #将该batch内的T帧原封不动的直接cat到结果集中
             
             mirrorImage = torch.cat([mirrorImage,image[l].view(1,T,C,H,W)],dim=0) #[1,T,C,H,W]
-        #########################################################################
+
         elif(label_B[l]==1):
-            #当前batch被选中，此时，在该batch内部随机挑选frame
-            #选中的batch的1-level标签
-            #创建保存每个B内结果的集合
             result_B = torch.tensor([]).cuda()
-            #生成2-level标签
-            #生成T个2级标签
             labelt_2 = [random.randint(0, 1) for _ in range(T)] 
             label_t_2 = torch.tensor(labelt_2).cuda()
 
-            #遍历label_t_2
             for t in range(T):
                 if(label_t_2[t]==0):
-                    #未选中的F,此时只需要进行不带通道变换的轴旋转变换,具体是Z还是H，需要3级标签来进行具体的实现
-                    #3-level标签
                     labelt_3 = [random.randint(0, 1) for _ in range(1)]
                     label_t_3 = torch.tensor(labelt_3).cuda()
                     if(label_t_3[0]==0):
                         #Z
-                        #首先直接修改1级标签
                         ll = [1]
                         l_l = torch.tensor(ll).cuda() #list->tensor
                         label_1_t = torch.cat([label_1_t,l_l])
-                        #进行变换
                         result_t = TmirrorZong(image[l][t]) #image[l][t]->[C,H,W]
                         result_B = torch.cat([result_B,result_t.view(1,C,H,W).cuda()])
 
@@ -96,21 +82,16 @@ def RBRF(images):#image->[BT,C,H,W]
                         ll = [1]
                         l_l = torch.tensor(ll).cuda()
                         label_1_t = torch.cat([label_1_t,l_l])
-                        #进行变换
                         result_t = TmirrorHeng(image[l][t]) #image[l][t]->[C,H,W]
                         result_B = torch.cat([result_B,result_t.view(1,C,H,W).cuda()])
                 elif(label_t_2[t]==1):
-                    #选中的batch，
-                    #3-level标签
                     labelt_3 = [random.randint(0, 1) for _ in range(1)]
                     label_t_3 = torch.tensor(labelt_3).cuda()
                     if(label_t_3[0]==0):
                         #Z
-                        #首先直接修改1级标签
                         ll = [1]
                         l_l = torch.tensor(ll).cuda()
                         label_1_t = torch.cat([label_1_t,l_l])
-                        #进行变换
                         tt = TmirrorZong(image[l][t]).view(1,C,H,W)
                         result_t = torch.tensor([]).cuda()
                         index = list(range(0, C, 1))
@@ -122,7 +103,6 @@ def RBRF(images):#image->[BT,C,H,W]
                         ll = [1]
                         l_l = torch.tensor(ll).cuda()
                         label_1_t = torch.cat([label_1_t,l_l])
-                        #进行变换
                         tt = TmirrorHeng(image[l][t]).view(1,C,H,W)
                         result_t = torch.tensor([]).cuda()
                         index = list(range(0, C, 1))
