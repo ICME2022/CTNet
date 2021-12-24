1addc71608f2b090b2a3cd8ae642b216e7b8b5bf
ICME2022 <96417027+ICME2022@users.noreply.github.com>
2021-12-24 20:54:43 +0800

    Update models.py

--- ops/models.py
+++ ops/models.py
@@ -1,7 +1,4 @@
-# Code for "TDN: Temporal Difference Networks for Efficient Action Recognition"
-# arXiv: 2012.10071
-# Limin Wang, Zhan Tong, Bin Ji, Gangshan Wu
-# tongzhan@smail.nju.edu.cn
+
 
 from torch import nn
 
